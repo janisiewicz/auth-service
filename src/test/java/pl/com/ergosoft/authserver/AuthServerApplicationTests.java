bfa9b8e798c55658c6183ba6db0234fe63167ef9
tjanisiewicz <tomasz.janisiewicz@dxc.com>
2020-05-01 08:46:59 +0200

    Initial commit

---
+++ src/test/java/pl/com/ergosoft/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.ergosoft.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
